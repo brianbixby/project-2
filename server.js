14cde1a30080fb2ad43197b9cb5e12f8c43b5a28
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-05-10 15:04:48 -0700

    working on socket with c4

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const allRoutes = require("./controllers");
 const session = require("express-session");
+const { v4: uuidv4 } = require('uuid');
 const http = require('http');
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -14,7 +15,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 // Requiring our models for syncing
-const { Friend, Game, GameInstance, Ranking, User } = require("./models");
+// const { Friend, Game, GameInstance, Ranking, User } = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -35,8 +36,10 @@ app.set('view engine', 'handlebars');
 
 app.use("/", allRoutes);
 
-const openGames = [];
-const inUse = [];
+const openGamesTTC = [];
+const inUseTTC = [];
+const openGamesC4 = [];
+const inUseC4 = [];
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -47,12 +50,52 @@ io.on('connection', (socket) => {
     io.emit('chat message', msg);
   });
 
-  socket.on('joinSpecificGame', room => {
-    socket.join(room);
+  // socket.on('joinSpecificGame', room => {
+  //   socket.join(room);
+  // })
+  // socket.on('joinAnyGame', room => {
+  //   socket.join(room);
+  // })
+  socket.on('joinGame', data => {
+    console.log('server join game');
+    let game;
+    if (data.gameID == 1) {
+      if (openGamesTTC.length) {
+        game = openGamesTTC.pop();
+        game.players = 2;
+        game.player2 = data.userID;
+        inUseTTC.push(game);
+      } else {
+        game = {id: uuidv4(), players: 1};
+        game.player1 = data.userID;
+        openGamesTTC.push(game);
+      }
+    } else if (data.gameID == 2) {
+      if (openGamesC4.length) {
+        game = openGamesC4.pop();
+        game.players = 2;
+        game.player2 = data.userID;
+        inUseC4.push(game);
+      } else {
+        game = {id: uuidv4(), players: 1};
+        game.player1 = data.userID;
+        openGamesC4.push(game);
+      }
+    }
+    socket.join(game.id);
+    io.to(game.id).emit('joinedGame', game);
   })
-  socket.on('joinAnyGame', room => {
-    socket.join(room);
-  })
+  // to do:
+  // have function that handles move then emits back to both clients
+  // socket.on("playerMadeMove", data => {
+  //   io.to(game.id).emit('playerMadeMove', game);
+  // })
+
+
+  // socket.on('c4move', data => {
+    // io.to(data.game.id).emit('joinGame', data);
+    // passes the back the data to a function on client that will update the board for both players
+  // })
 });
 
 sequelize.sync({ force: false }).then(function () {
