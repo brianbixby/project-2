d5cc99e6975420fca0222da7a1934dfb89f1bed2
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-05-10 10:09:26 -0700

    starting socket

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const allRoutes = require("./controllers");
 const session = require("express-session");
+const http = require('http');
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 // Requiring our models for syncing
 const { Friend, Game, GameInstance, Ranking, User } = require("./models");
@@ -31,8 +35,28 @@ app.set('view engine', 'handlebars');
 
 app.use("/", allRoutes);
 
+const openGames = [];
+const inUse = [];
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('joinSpecificGame', room => {
+    socket.join(room);
+  })
+  socket.on('joinAnyGame', room => {
+    socket.join(room);
+  })
+});
+
 sequelize.sync({ force: false }).then(function () {
-  app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+  server.listen(PORT, function () {
+    console.log("Server listening on PORT " + PORT);
   });
 });
