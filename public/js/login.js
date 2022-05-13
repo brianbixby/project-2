33b0ff0c1bfa6caf4d923747a19e1d3d0140121b
Hannah Callison <hannahcallison1007@gmail.com>
2022-05-12 22:07:21 -0700

    redoing styling to flex

--- public/js/login.js
+++ public/js/login.js
@@ -6,9 +6,16 @@ if (loginEl) {
     loginEl.addEventListener("submit", async (e) => {
         try {
             e.preventDefault();
-            const userObj = { email: document.querySelector("#loginEmail").value, password: document.querySelector("#loginPassword").value };
-            const data = await fetch("/api/users/login", { method: "POST", body: JSON.stringify(userObj), headers: { "Content-Type": "application/json" } });
-            if (data.ok) document.location.replace("/games");
+            const userObj = { 
+                email: document.querySelector("#loginEmail").value, 
+                password: document.querySelector("#loginPassword").value };
+            const data = await fetch("/api/users/login", { 
+                method: "POST", 
+                body: JSON.stringify(userObj), 
+                headers: { "Content-Type": "application/json" } });
+            if (data.ok) {
+                    location.href="/games"
+            }
         }
         catch (err) {
             console.log("error: ", err);
@@ -24,7 +31,9 @@ if (signupEl) {
             e.preventDefault();
             const userObj = { email: document.querySelector("#signupEmail").value, password: document.querySelector("#signupPassword").value };
             const data = await fetch("/api/users/", { method: "POST", body: JSON.stringify(userObj), headers: { "Content-Type": "application/json" }});
-            if (data.ok) document.location.replace("/games");
+            if (data.ok) {
+                location.href="/games";
+            }
         } catch (err) {
             console.log("error: ", err);
         }
